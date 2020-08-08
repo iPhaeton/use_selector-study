80d254b6efc045af35a61e8cef5242aec3f3225b
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2020-08-08 15:40:03 +0300

    Create memoized selector

--- src/selectors.js
+++ src/selectors.js
@@ -1,6 +1,10 @@
+import { createSelector } from 'reselect';
+
 export const getGoods = state => state.goods.list;
 
-export const getSortedGoods = state => {
-    const goods = getGoods(state);
-    return goods.slice().sort((a, b) => a.price - b.price);
-};
+export const getSortedGoods = createSelector(
+    getGoods,
+    goods => {
+        return goods.slice().sort((a, b) => a.price - b.price)
+    },
+);
